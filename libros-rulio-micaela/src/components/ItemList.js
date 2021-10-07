9680ac82f1fa21be570b87f69c3e956a96d91f8f
micaela rulio <micaelaagostinarulio@gmail.com>
2021-10-07 18:49:25 -0300

    Promises nuevo

--- libros-rulio-micaela/src/components/ItemList.js
+++ libros-rulio-micaela/src/components/ItemList.js
@@ -1,94 +1,14 @@
-import React, {useState,useEffect} from 'react';
+import React from 'react'
 import Item1 from "./Item1";
 
-
-
-const productos = 
-        [
-        {
-            id:1,
-            titulo: "Orgullo y prejuicio",
-            precio: "$ 2000",
-            url:"./imagenes/orgullo.jpg",
-            autor: "Jane Austin",
-            descripcion: "....."
-        },
-        {
-            id:2,
-            titulo: "Jane Eyre",
-            precio: "$ 1500",
-            url:"./imagenes/jane.jpg",
-            autor: "Charlote Bonte",
-            descripcion: "....."
-        },
-        {
-            id:3,
-            titulo: "Dracula",
-            precio: "$ 1000",
-            url:"./imagenes/dracula.jpg",
-            autor: "Bram Stoker",
-            descripcion: "....."
-        },
-        
-    ];
-
-
-function comprobarError(){
-    return (Math.random() < 0.2);
-    }
-
-
-function crearPromesa() {
-    return new Promise((resolve, reject) => {  
-    setTimeout(
-        function(){                
-            const error = comprobarError();           
-            if(!(error)){      
-                resolve(productos);  
-            }
-            else { 
-                reject( new Error("Error obteniendo los datos"));
-            }
-        }, 
-        2000);      
-    });     
-}
-
-
-function ItemList(){
-
-    const [items,setItems] = React.useState(null);
-    const [estado,setEstado] = useState('Cargando...');
-    
-    useEffect(() => {    
-        let pedidoDatos = crearPromesa();
-
-        pedidoDatos
-        .then( function(items_promise){
-            setItems(items_promise);     
-            setEstado('Listo');           
-            console.log(items_promise);
-        })      
-
-        .catch( function(errores){
-            console.log(errores);   
-            setEstado('Error detectado');       
-        })
-        .finally( ()=>{
-                console.log('Promesa terminada');
-            }
-        )
-    }, []);
-
-
+function ItemList({info}) {
     return (
         <div id = "contenedorLibros">
-                <div><h3>{estado}</h3></div>  
             <hr/>         
             
             <div id="cont">
-                {(items !== null) && 
-                items.map( (itm,index) =>
+                {(info!== null) && 
+                info.map( (itm,index) =>
                     <Item1
                         key={index}
                         titulo={itm.titulo}
@@ -100,8 +20,9 @@ function ItemList(){
             )}        
             </div>           
         </div>
-    )  
+    )
 }
 
-export default ItemList;
+export default ItemList
+
 
--- libros-rulio-micaela/src/container/ItemListContainer.js
+++ libros-rulio-micaela/src/container/ItemListContainer.js
@@ -1,9 +1,88 @@
 import ItemList from "../components/ItemList";
+import React, {useState,useEffect} from 'react';
+
+
+
+
+const productos = 
+        [
+        {
+            id:1,
+            titulo: "Orgullo y prejuicio",
+            precio: "$ 2000",
+            url:"./imagenes/orgullo.jpg",
+            autor: "Jane Austin",
+            descripcion: "....."
+        },
+        {
+            id:2,
+            titulo: "Jane Eyre",
+            precio: "$ 1500",
+            url:"./imagenes/jane.jpg",
+            autor: "Charlote Bonte",
+            descripcion: "....."
+        },
+        {
+            id:3,
+            titulo: "Dracula",
+            precio: "$ 1000",
+            url:"./imagenes/dracula.jpg",
+            autor: "Bram Stoker",
+            descripcion: "....."
+        },
+        
+    ];
+
+
+function comprobarError(){
+    return (Math.random() < 0.2);
+    }
+
+
+function crearPromesa() {
+    return new Promise((resolve, reject) => {  
+    setTimeout(
+        function(){                
+            const error = comprobarError();           
+            if(!(error)){      
+                resolve(productos);  
+            }
+            else { 
+                reject( new Error("Error obteniendo los datos"));
+            }
+        }, 
+        2000);      
+    });     
+}
 
 
 
 
 const ItemListContainer = (props) => {
+    const [items,setItems] = React.useState(null);
+    const [estado,setEstado] = useState('Cargando...');
+    
+    useEffect(() => {    
+        let pedidoDatos = crearPromesa();
+
+        pedidoDatos
+        .then( function(items_promise){
+            setItems(items_promise);     
+            setEstado('Listo');           
+            console.log(items_promise);
+        })      
+
+        .catch( function(errores){
+            console.log(errores);   
+            setEstado('Error detectado');       
+        })
+        .finally( ()=>{
+                console.log('Promesa terminada');
+            }
+        )
+    }, []);
+    
+    
     return  (
             <div>
                 <div id = "ingreso">
@@ -12,7 +91,7 @@ const ItemListContainer = (props) => {
                     <p>Email: {props.email}</p>
                 </div>
                 <div className = "contenedorLibros">
-                    <ItemList/>                    
+                    <ItemList info = {items}/>                    
                 </div>
             </div>
 );
